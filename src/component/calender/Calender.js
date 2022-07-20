668ec3920d65c96d599141d363dc2b69270526f8
TaiLinh <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2022-07-20 11:21:36 +0700

    component calender

---
+++ src/component/calender/Calender.js
@@ -0,0 +1,32 @@
+import FooterCalender from "./FooterCalender"
+import HeaderCalender from "./HeaderCalender"
+import ModelCalender from "./ModelCalender"
+import SitebarCalender from "./SitebarCalender"
+import ViewCalender from "./ViewCalender"
+
+function Calender() {
+  return (
+    <div class="preloader-visible" data-barba="wrapper">
+      <div class="barba-container" data-barba="container">
+        <main class="main-content">
+
+          <HeaderCalender />
+
+          <div class="content-wrapper js-content-wrapper">
+            <div class="dashboard -home-9 js-dashboard-home-9">
+              <SitebarCalender />
+              < ViewCalender />
+            </div>
+          </div>
+          
+        </main>
+        <FooterCalender />
+        <ModelCalender />
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Calender
---
+++ src/component/calender/ViewMonth.js
@@ -0,0 +1,422 @@
+function ViewMonth() {
+    return (
+        <div class="col-xl-3 col-lg-3">
+                      <div class="row y-gap-30">
+
+                        <div class="col-12">
+                          <div class="pt-20 pb-30 px-30 rounded-16 bg-white -dark-bg-dark-1 shadow-4">
+                            <h5 class="text-17 fw-500 mb-30">Monthly view</h5>
+
+                            <div class="">
+                              <div class="d-flex x-gap-10 y-gap-10 justify-center items-center pb-5">
+
+                                <div class="text-14 lh-11 text-dark-1 fw-500">May 2022</div>
+
+                              </div>
+
+                              <div class="d-flex x-gap-10 y-gap-10 flex-wrap pb-5">
+
+                                <div class="w-1/7">
+                                  <div class="d-flex justify-center size-30 items-center">
+                                    <div class="text-13 text-dark-1">SUN</div>
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+                                  <div class="d-flex justify-center size-30 items-center">
+                                    <div class="text-13 text-dark-1">MON</div>
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+                                  <div class="d-flex justify-center size-30 items-center">
+                                    <div class="text-13 text-dark-1">TUE</div>
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+                                  <div class="d-flex justify-center size-30 items-center">
+                                    <div class="text-13 text-dark-1">WED</div>
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+                                  <div class="d-flex justify-center size-30 items-center">
+                                    <div class="text-13 text-dark-1">THU</div>
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+                                  <div class="d-flex justify-center size-30 items-center">
+                                    <div class="text-13 text-dark-1">FRI</div>
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+                                  <div class="d-flex justify-center size-30 items-center">
+                                    <div class="text-13 text-dark-1">SAT</div>
+                                  </div>
+                                </div>
+
+                              </div>
+
+                              <div class="d-flex x-gap-10 y-gap-10 flex-wrap">
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14">28</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14">29</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14">30</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">1</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">2</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">3</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">4</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">5</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">6</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">7</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">8</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">9</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">10</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">11</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">12</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">13</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">14</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">15</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center items-center size-30 bg-purple-1 rounded-full">
+
+
+                                    <a href="#" class="text-14 text-white">16</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">17</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">18</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">19</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">20</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center items-center size-30 bg-purple-1 rounded-full">
+
+
+                                    <a href="#" class="text-14 text-white">21</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">22</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">23</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">24</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">25</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">26</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">27</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14">28</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14">29</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14">30</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+
+
+                                    <a href="#" class="text-14 text-dark-1">31</a>
+
+                                  </div>
+                                </div>
+
+                                <div class="w-1/7">
+
+                                  <div class="d-flex justify-center size-30 items-center">
+                                    <a href="#" class="text-14 text-dark-1">1</a>
+
+                                  </div>
+                                </div>
+
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+    )
+}
+
+export default ViewMonth
