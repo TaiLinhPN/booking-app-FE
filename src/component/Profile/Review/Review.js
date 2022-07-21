07fb1a0f61ad9c45ef06c14a843a0af8d9bfcbe7
LazyyNguyen <96377381+khanhlinh112002@users.noreply.github.com>
2022-07-21 08:15:21 +0700

    edit size header

--- src/component/Profile/Review/Review.js
+++ src/component/Profile/Review/Review.js
@@ -1,7 +1,7 @@
 import React,{ Component} from "react";
 import Menu from "../Menu/Menu";
 // import '../../../js/extra';
-export default class App extends Component {
+export default class Review extends Component {
     render() 
     {
       return(
