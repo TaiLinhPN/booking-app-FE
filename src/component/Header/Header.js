b7f74ffb660e4a3fefc5664f808f2a0dcf2ef495
Phuong0210 <phuong.ho021021@gmail.com>
2022-07-21 08:59:30 +0700

    change header

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -10,7 +10,7 @@ function Header() {
               <div className="header-left">
                 <div className="header__logo ">
                   <a data-barba href="index.html">
-                    <img src="img/general/logop.png" alt="logo" />
+                    <img className="header__img" src="img/general/logop.png" alt="logo" />
                   </a>
                 </div>
                 <div className="header__explore text-green-1 ml-60 xl:ml-30 xl:d-none">
