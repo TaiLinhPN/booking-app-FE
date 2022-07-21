4c7c69cdd9efebe0b8060d073193032a989cfef3
tronngo <tron.ngo23@student.passerellesnumeriques.org>
2022-07-21 09:50:03 +0700

    divide component finished 21/7

--- freecourse/src/component/ruless/Rules.js
+++ freecourse/src/component/ruless/Rules.js
@@ -1,3 +1,5 @@
+import RuleChilde from "./RulesChilde/RulesChilde";
+
 function Rules() {
   return (
     <>
@@ -23,79 +25,7 @@ function Rules() {
       </section>
       <section class="layout-pb-lg">
         <div data-anim-wrap="" class="container animated">
-          <div class="row y-gap-30 justify-between pt-90 lg:pt-50">
-            <div class="col-lg-4 col-md-6">
-              <div class="py-40 px-45 rounded-16 bg-light-4">
-                <div class="d-flex justify-center items-center size-70 rounded-full bg-white">
-                  <img src="img/help-center/1.svg" alt="icon" />
-                </div>
-                <h4 class="text-20 lh-11 fw-500 mt-25">Course ruless</h4>
-                <p class="mt-10">
-                  Lorem ipsum is placeholder text commonly used in site.
-                </p>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6">
-              <div class="py-40 px-45 rounded-16 bg-light-4">
-                <div class="d-flex justify-center items-center size-70 rounded-full bg-white">
-                  <img src="img/help-center/2.svg" alt="icon" />
-                </div>
-                <h4 class="text-20 lh-11 fw-500 mt-25">Account / Profile</h4>
-                <p class="mt-10">
-                  Lorem ipsum is placeholder text commonly used in site.
-                </p>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6">
-              <div class="py-40 px-45 rounded-16 bg-light-4">
-                <div class="d-flex justify-center items-center size-70 rounded-full bg-white">
-                  <img src="img/help-center/3.svg" alt="icon" />
-                </div>
-                <h4 class="text-20 lh-11 fw-500 mt-25">Troubleshooting</h4>
-                <p class="mt-10">
-                  Lorem ipsum is placeholder text commonly used in site.
-                </p>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6">
-              <div class="py-40 px-45 rounded-16 bg-light-4">
-                <div class="d-flex justify-center items-center size-70 rounded-full bg-white">
-                  <img src="img/help-center/4.svg" alt="icon" />
-                </div>
-                <h4 class="text-20 lh-11 fw-500 mt-25">Purchase / Refunds</h4>
-                <p class="mt-10">
-                  Lorem ipsum is placeholder text commonly used in site.
-                </p>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6">
-              <div class="py-40 px-45 rounded-16 bg-light-4">
-                <div class="d-flex justify-center items-center size-70 rounded-full bg-white">
-                  <img src="img/help-center/5.svg" alt="icon" />
-                </div>
-                <h4 class="text-20 lh-11 fw-500 mt-25">Course Taking</h4>
-                <p class="mt-10">
-                  Lorem ipsum is placeholder text commonly used in site.
-                </p>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6">
-              <div class="py-40 px-45 rounded-16 bg-light-4">
-                <div class="d-flex justify-center items-center size-70 rounded-full bg-white">
-                  <img src="img/help-center/6.svg" alt="icon" />
-                </div>
-                <h4 class="text-20 lh-11 fw-500 mt-25">Mobile General</h4>
-                <p class="mt-10">
-                  Lorem ipsum is placeholder text commonly used in site.
-                </p>
-              </div>
-            </div>
-          </div>
+          <RuleChilde />
         </div>
       </section>
     </>
