74fd3c26a38013b8dd5a430afdfd771c632fbc7b
LazyyNguyen <96377381+khanhlinh112002@users.noreply.github.com>
2022-07-28 08:18:32 +0700

    change route2

--- src/Routes/routes_2.js
+++ src/Routes/routes_2.js
@@ -1,39 +1,52 @@
 import Review from '../component/Profile/Review/Review';
+import Setting from '../component/Profile/Setting/Setting';
 import Course from '../component/Profile/Course/Course';
 import CurrentProfile from '../component/Profile/Account/CurrentProfile';
 import ForgetPassword from '../component/Profile/ForgetPassword/ForgetPassword';
 import EditProfile from '../component/Profile/Account/EditProfile';
 const routes_2 =[
+
     {
         path : '/Course',
         exact : true,
         main : ({history})=> <Course history={history} />
+
     },
     
 {
     path : '/Review',
-	exact : true,
+
 	main : ({history})=> <Review history={history} />
 }
-,{
+,
+{
+    path : '/Setting',
+
+	main : ({history})=> <Setting history={history} />
+},{
+    path: '/Profile',
+
+    main: ({history})=> <CurrentProfile history={history} />
+},
+{
     path: '/CurrentProfile',
-    exact: true,
+
     main: ({history})=> <CurrentProfile history={history} />
 },
 {
 
     path: '/ForgetPassword',
-    exact : true,
+
     main : ({history})=> <ForgetPassword history={history} />   
 }
 ,
 {
 
     path: '/EditProfile',
-    exact : true,
+
     main : ({history})=> <EditProfile history={history} />   
 }
 
 
 ]
-export default routes_2;
+export d
